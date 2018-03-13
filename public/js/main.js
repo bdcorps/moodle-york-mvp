83ed4d6b15094c3c478b4d5e1ed9468f7027e7b8
Minho Lee <minho.lee.93@hotmail.com>
2018-03-13 13:47:14 -0400

    set up basic structure and using NodeRed as backend

---
+++ public/js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+	let date = new Date();
+
+	let dd = date.getDate();
+	let mm = date.getMonth() + 1;
+	let yy = date.getFullYear();
+
+	if (dd < 10) {
+		dd = '0' + dd;
+	}
+	if (mm < 10) {
+		mm = '0' + mm;
+	}
+
+	$("#currentDate").html(mm + ' / ' + dd + ' / ' + yy);
+	console.log(message);
+	$("#chatbox-body").append("<p>"+message+"</p>");
+});
+
+
+
+$("#submit-conversation").on('submit', function(e) {
+	// axios({
+	// 	method: 'post',
+	// 	url: 'https://moodle-lite-mvp-nodered.mybluemix.net/testing',
+	// 	data: {
+	// 		message: $("#userInput").val(),
+	// 	}
+	// })
+	// .then(function(res) {
+	// 	console.log(res);
+	// });
+	
+});
